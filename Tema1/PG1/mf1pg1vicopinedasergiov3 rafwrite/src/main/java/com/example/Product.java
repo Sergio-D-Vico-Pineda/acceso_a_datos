5292d926176aa103872eb5b556f922c3655dd16f
Scarpy19_ <nootravez19@gmail.com>
2024-11-12 13:48:07 +0100

    raf read

--- Tema1/PG1/mf1pg1vicopinedasergiov3 rafwrite/src/main/java/com/example/Product.java
+++ Tema1/PG1/mf1pg1vicopinedasergiov3 rafwrite/src/main/java/com/example/Product.java
@@ -1,11 +1,12 @@
 package com.example;
 
 public class Product {
+
     String name;
     double price;
     int quantity;
     int id;
-    
+
     public String getName() {
         return name;
     }
@@ -34,14 +35,14 @@ public class Product {
     }
 
     public String showInfo() {
-        // rellenar con ceros hasta el tamaño de 15 caracteres
+        // rellenar con ceros hasta el tamaño de x caracteres
         String id = String.format("%-3d", this.id);
         String name = String.format("%-15s", this.name);
         String price = String.format("%-10.2f", this.price);
         String quantity = String.format("%-5d", this.quantity);
         return id + "-| Nombre: " + name + " | Precio: " + price + " | Cantidad: " + quantity;
     }
-    
+
     @Override
     public String toString() {
         String id = String.format("%-3d", this.id);
@@ -51,9 +52,11 @@ public class Product {
         return id + ":" + name + ":" + price + ":" + quantity + ";";
     }
 
-    public static Product fromString(String line) { // 1:mouse:11.0:2
+    public static Product fromString(String line) { // 0  :mouse          :15,40     :1
         String[] parts = line.split(":", 4);
-        return new Product(Integer.parseInt(parts[0]), parts[1], Double.parseDouble(parts[2]),
-                Integer.parseInt(parts[3]));
+        int id1 = Integer.parseInt(parts[0].trim());
+        double price1 = Double.parseDouble(parts[2].trim().replace(",", "."));
+        int quantity1 = Integer.parseInt(parts[3].trim());
+        return new Product(id1, parts[1].trim(), price1, quantity1);
     }
 }
