898ef6a3088c79d5c3dd318655f5514e993bbe43
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2025-01-24 17:19:22 +0100

    done

--- Tema3/inventariovicopinedasergio/src/main/java/com/example/Main.java
+++ Tema3/inventariovicopinedasergio/src/main/java/com/example/Main.java
@@ -1,7 +1,110 @@
 package com.example;
 
+import java.util.List;
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class Main {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int opcion;
+        do {
+            System.out.println("1. Añadir un producto");
+            System.out.println("2. Listar todos los productos");
+            System.out.println("3. Actualizar el stock de un producto");
+            System.out.println("4. Eliminar un producto");
+            System.out.println("5. Salir");
+            System.out.print("Opción: ");
+            try {
+                opcion = input.nextInt();
+            } catch (Exception e) {
+                opcion = -1;
+            }
+            input.nextLine();
+            switch (opcion) {
+                case 1 -> anadirProducto();
+                case 2 -> listarProductos();
+                case 3 -> actualizarProducto();
+                case 4 -> eliminarProducto();
+                case 5 -> salir();
+                default -> System.out.println("Opción incorrecta");
+            }
+            if (opcion != 5) {
+                System.out.println("");
+                System.out.print("Presiona ENTER para continuar...");
+                System.out.println("");
+                input.nextLine();
+            }
+        } while (opcion != 5);
+    }
+
+    public static void salir() {
+        System.out.println("Bye");
+        HibernateUtil.shutdown();
+    }
+
+    public static void anadirProducto() {
+        System.out.print("Nombre: ");
+        String nombre = input.next();
+        System.out.print("Precio: ");
+        double precio = input.nextDouble();
+        System.out.print("Stock: ");
+        int stock = input.nextInt();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Producto producto = new Producto(nombre, precio, stock);
+        session.persist(producto);
+
+        transaction.commit();
+        session.close();
+    }
+
+    public static void listarProductos() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Producto> productos = session.createQuery("from Producto",
+                Producto.class).list();
+
+        productos.forEach(System.out::println);
+
+        session.close();
+    }
+
+    public static void actualizarProducto() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction trans = session.beginTransaction();
+
+        System.out.print("ID del producto: ");
+        int id = input.nextInt();
+        Producto p = session.get(Producto.class, id);
+        if (p != null) {
+            System.out.print("Nuevo stock: ");
+            int nuevoStock = input.nextInt();
+            p.setStock(nuevoStock);
+            session.merge(p);
+            trans.commit();
+        } else {
+            System.out.println("No existe el producto.");
+        }
+        session.close();
+    }
+
+    public static void eliminarProducto() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction trans = session.beginTransaction();
+
+        System.out.print("ID del producto: ");
+        int id = input.nextInt();
+        Producto pEliminar = session.get(Producto.class, id);
+        if (pEliminar != null) {
+            session.remove(pEliminar);
+            trans.commit();
+        } else {
+            System.out.println("No existe el producto");
+        }
+        session.close();
     }
 }
---
+++ Tema3/inventariovicopinedasergio/src/main/java/com/example/Producto.java
@@ -0,0 +1,68 @@
+package com.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "producto")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "precio")
+    private double precio;
+
+    @Column(name = "stock")
+    private int stock;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
+    }
+}
